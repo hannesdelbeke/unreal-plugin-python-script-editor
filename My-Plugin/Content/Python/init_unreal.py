6fe699dc4b4ac0927f4e05d566b46b2e95f36fe5
hannes <hannesdelbeke@gmail.com>
2023-04-17 12:00:28 +0100

    move code inside plugin, setup plugin

--- My-Plugin/Content/Python/init_unreal.py
+++
@@ -1,2 +0,0 @@
-# code in init_unreal.py wil run on startup if the plugin is enabled
-print("Hello World! I am a Python script running on startup!")
---
+++ UnrealScriptEditor/Content/Python/init_unreal.py
@@ -0,0 +1,7 @@
+"""
+This file is executed on startup,
+and executes the script editor button creation.
+"""
+
+import unrealScriptEditor.startup
+unrealScriptEditor.startup.create_script_editor_button()
