c369e49f911334cbd674e37e5b4f8e99f1ce1b64
hannes <hannesdelbeke@gmail.com>
2023-06-07 20:07:40 +0100

    add startup to import for test file

--- debug_ui.py
+++ debug_ui.py
@@ -5,6 +5,8 @@ This is a debug script that can be run outside of unreal to test the UI.
 import sys
 from PySide2 import QtWidgets
 
+
+
 # mocking unreal allows us to run the UI outside of unreal,
 # preventing an exception on `import unreal` in unrealScriptEditor
 # run this before importing unrealScriptEditor
@@ -12,8 +14,10 @@ from unittest.mock import MagicMock
 mock_unreal = MagicMock()
 sys.modules["unreal"] = mock_unreal
 
+import pprint
+from unrealScriptEditor import startup
 from unrealScriptEditor import main
-
+pprint.pp(sys.path)
 
 # handle QApplication correctly, PythonScriptEditor attempts this but doesn't do _exec()
 APP = QtWidgets.QApplication.instance()
